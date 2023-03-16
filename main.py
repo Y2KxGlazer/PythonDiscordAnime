2744eb05da9e2ab190812fd98155857e90927258
Minijb Banks <bminijb@gmail.com>
2023-03-16 12:05:50 -0400

    Embeds Working(Need Ractor)

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 with open("token.txt") as f:
     TOKEN = f.readline().strip()
 
-
+folder_path = ""
 @bot.event
 async def on_ready():
     awaken_channel = 1055616703880503328  # This is the channel that the bot 'awakens' in
@@ -35,7 +35,7 @@ async def on_message(message):
             folder_path = r"C:\Users\bmini\Desktop\Pythonic Fluff\PythonDiscordAnime\Photos For Testing"
             file_name = rand.choice(os.listdir(folder_path))
             random_image_path = folder_path + r"\{}".format(file_name)
-            pic = discord.File(random_image_path, filename=file_name)
+            pic = discord.File(random_image_path, filename="image.jpg")
 
             color_thief_image = ColorThief(random_image_path)
             image_color = color_thief_image.get_color(quality=1)
@@ -43,13 +43,12 @@ async def on_message(message):
 
             user_embed = discord.Embed(color=color_from_image, title="test",
                                        type='rich')
-            user_embed.set_image(url=f"attachment://{random_image_path}")
+            user_embed.set_image(url=f"attachment://image.jpg")
             await msg_channel.send(embed=user_embed, file=pic)
 
 
         #TODO: set up bot prefix..... skipping
 
-        #TODO: embed image
 
         #TODO: Input MAL
 
