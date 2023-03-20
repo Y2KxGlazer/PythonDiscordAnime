40eb75e55d403e3109a79d050747f6d8ec34bf4f
Minijb Banks <bminijb@gmail.com>
2023-03-20 13:57:48 -0400

    Embeds Working(Need Ractor)

--- Embed.py
+++ Embed.py
@@ -1,10 +1,41 @@
+import io
+import os, random as rand
 import discord
-# Embed builder class
+from colorthief import ColorThief
+from PIL import Image
 
-# The strip on the embed
-# user_embed = discord.Embed(color=discord.Color.dark_red(), title="test",
-#                            type='rich')
-# await channel.send(embed=user_embed)
+# Embed builder module
+
+
+def embed_builder():
+    pass
+
+
+def send_embed_message():
+    pass
+
+
+def send_rand_local_message():
+    folder_path = r"C:\Users\bmini\Desktop\Pythonic Fluff\PythonDiscordAnime\Photos For Testing"
+    file_name = rand.choice(os.listdir(folder_path))
+    random_image_path = folder_path + r"\{}".format(file_name)
+
+    with Image.open(random_image_path) as rand_image:
+        resized_random_image = rand_image.resize((256, 256))
+        arr = io.BytesIO()
+        resized_random_image.save(arr)
+        arr.seek(0)
+
+    pic = discord.File(arr, filename="image.jpg")
+
+
+    image_color = color_thief_image.get_color(quality=1)
+    color_from_image = discord.Color.from_rgb(image_color[0], image_color[1], image_color[2])
+
+    user_embed = discord.Embed(color=color_from_image, title="test",
+                               type='rich')
+    user_embed.set_image(url=f"attachment://image.jpg")
+    await msg_channel.send(embed=user_embed, file=pic)
 
 #Todo get the most dominate color for the color in
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import os, random as rand
-from colorthief import ColorThief
+
+
 import asyncio
 
 
@@ -31,20 +31,10 @@ async def on_message(message):
             await msg_channel.send("Nope")
 
         else:
+            pass
+            # Lower the resolution of image so color theif can work faster
 
-            folder_path = r"C:\Users\bmini\Desktop\Pythonic Fluff\PythonDiscordAnime\Photos For Testing"
-            file_name = rand.choice(os.listdir(folder_path))
-            random_image_path = folder_path + r"\{}".format(file_name)
-            pic = discord.File(random_image_path, filename="image.jpg")
 
-            color_thief_image = ColorThief(random_image_path)
-            image_color = color_thief_image.get_color(quality=1)
-            color_from_image = discord.Color.from_rgb(image_color[0], image_color[1], image_color[2])
-
-            user_embed = discord.Embed(color=color_from_image, title="test",
-                                       type='rich')
-            user_embed.set_image(url=f"attachment://image.jpg")
-            await msg_channel.send(embed=user_embed, file=pic)
 
 
         #TODO: set up bot prefix..... skipping
