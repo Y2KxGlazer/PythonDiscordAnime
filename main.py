ec35cb815aafade9d8c8713400b719ef0b79b6ce
Y2KxGlazer <121055498+Y2KxGlazer@users.noreply.github.com>
2024-05-26 00:06:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ with open("token.txt") as f:
 folder_path = ""
 @bot.event
 async def on_ready():
-    awaken_channel = 1055616703880503328  # This is the channel that the bot 'awakens' in
+    awaken_channel =   # This is the channel that the bot 'awakens' in ( Right-Click Channel -> Copy Channel ID)
     print('Logged in as {0.user}'.format(bot))
     channel = bot.get_channel(awaken_channel)
     await channel.send(f'Hello, world!')
@@ -27,9 +27,9 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     msg_channel = message.channel
-    if message.author.name == "George Orwell" and message.author.discriminator == "3046":
+    if message.author.name == "Test" and message.author.discriminator == "1234":
 
-        if msg_channel.id != 1055616703880503328:
+        if msg_channel.id != : # Enter Valid channels
             await msg_channel.send("Nope")
 
         else:
